474406c3b4df40d7cef05fe01adbb26c74223feb
amaan35 <amaanlm35@gmail.com>
2023-10-26 21:16:26 +0530

    variables : mutable and immutable - var, val

---
+++ Variables/src/main/kotlin/Variables.kt
@@ -0,0 +1,15 @@
+fun main(){
+    var name = "Syed Amaan Ali"
+    val userId = 123
+    var userAddress = "New Delhi"
+
+    println(name)
+    println(userId)
+    println(userAddress)
+    // String concatenation using template
+    println("User name is "+name)
+    println("User name is $name")
+    var a = 45
+    var b = 89
+    println("Sum of $a and $b is ${a+b}")
+}
