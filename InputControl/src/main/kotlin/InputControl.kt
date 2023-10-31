e986d7fa85d2781ae3968b347af60f70e79c2c4c
amaan35 <amaanlm35@gmail.com>
2023-10-31 10:06:33 +0530

    Taking Input

---
+++ InputControl/src/main/kotlin/InputControl.kt
@@ -0,0 +1,11 @@
+fun main(){
+    println("Enter your name : ")
+    val name = readln()
+    println("hello, $name")
+    println("Enter a number : ")
+    val number = readln().toInt()
+    println("Number : $number")
+    println("Enter a number : ")
+    val number2 = readln().toDouble()
+    println("Number : $number2")
+}
